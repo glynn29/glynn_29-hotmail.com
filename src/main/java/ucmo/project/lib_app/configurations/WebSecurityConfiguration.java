815b499e45e7288bd8487280d13078a8bef74e8d
glynn29 <glynn_29@hotmail.com>
2020-05-27 20:49:42 -0500

    deployment?

---
+++ src/main/java/ucmo/project/lib_app/configurations/WebSecurityConfiguration.java
@@ -0,0 +1,65 @@
+package ucmo.project.lib_app.configurations;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import ucmo.project.lib_app.services.UserService;
+
+import javax.sql.DataSource;
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    DataSource dataSource;
+
+    private static final String timeFormat = "hh:mm:ss";
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.serializers(new LocalTimeSerializer(DateTimeFormatter.ofPattern(timeFormat)));
+        };
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/resources/**").permitAll()
+                .antMatchers("/").hasAnyRole("USER","PROCTOR","ADMIN")
+                .antMatchers("/checkin").hasAnyRole("USER","PROCTOR","ADMIN")
+                .antMatchers("/list").hasAnyRole("PROCTOR","ADMIN")
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").defaultSuccessUrl("/",true)
+                .permitAll().and()
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID").permitAll();
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+}
